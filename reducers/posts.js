af504da2af2c989b79ad28072bbc2df1a2fa37f1
Blake A. Long <blake.a.long@gmail.com>
2021-11-14 13:29:33 -0700

    second commit

---
+++ reducers/posts.js
@@ -0,0 +1,18 @@
+// function posts(state = [], action) {
+//   switch (action.type) {
+//     case FETCH_ALL:
+//       return state
+      
+//     case FETCH_ALL:
+//       return state
+
+//     case FETCH_ALL:
+//       return state
+
+//     case FETCH_ALL:
+//       return state
+      
+//     default:
+//       return state
+//   }
+// }
